244064ec33179c0abc5ab69cff78adbf1bcc41a4
QuchengRep1 <QuchengRep_1@163.com>
2021-02-22 11:30:25 +0800

    20210222 11:15, at Office.

--- RPC_01/src/main/java/com/qucheng/rpc03/Client.java
+++ RPC_01/src/main/java/com/qucheng/rpc03/Client.java
@@ -4,10 +4,11 @@ import com.qucheng.rpc.common.IUserService;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Xiao AI Student ~")
+        System.out.println("Xiao AI Student ~");
         IUserService service = Stub.getStub();
         System.out.println(service.findUserById(123));
         System.out.println(service.findUserById(456));
         System.out.println("Xiao AI Student ~");
+        System.out.println("Xiao AI 同学 ~");
     }
 }
