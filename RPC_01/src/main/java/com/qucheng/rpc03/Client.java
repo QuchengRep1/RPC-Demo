db98c2c43fd9d68d0fd8f702bf85c3ecc5468213
QuchengRep1 <75408310+QuchengRep1@users.noreply.github.com>
2021-02-22 11:29:26 +0800

    Update Client.java

--- RPC_01/src/main/java/com/qucheng/rpc03/Client.java
+++ RPC_01/src/main/java/com/qucheng/rpc03/Client.java
@@ -4,7 +4,7 @@ import com.qucheng.rpc.common.IUserService;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Xiao AI Student ~")
+        System.out.println("Xiao AI 同学 ~")
         IUserService service = Stub.getStub();
         System.out.println(service.findUserById(123));
         System.out.println(service.findUserById(456));
